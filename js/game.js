84e32b4761238464dfe26a7ca54a8d14f70d928c
Julia Pereira Teixeira <juliapt95@gmail.com>
2022-07-31 12:02:03 -0300

    v1

--- js/game.js
+++ js/game.js
@@ -23,7 +23,7 @@ const checkEndGame = () => {
 
     if (disabledCards.length == 20){
         clearInterval(this.loop);
-        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}s!`);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML} s!`);
     }
 }
 
